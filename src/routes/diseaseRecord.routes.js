1cf7b81918cd5b55673310313503d79256d58095
Viet Hieu Dinh <59277071+hieudepoet@users.noreply.github.com>
2025-06-20 20:40:31 +0700

    update diseaseRecord

--- src/routes/diseaseRecord.routes.js
+++ src/routes/diseaseRecord.routes.js
@@ -1,7 +1,9 @@
 import express from "express";
 import {
   createDiseaseRecord,
-  getDiseaseOfStudent,
+  getDiseaseRecordsOfStudent,
+  getChronicDiseaseRecordsOfStudent,
+  getInfectiousDiseaseRecordsOfStudent,
   getAllInfectiousDiseaseRecords,
   getAllChronicDiseaseRecords,
   updateDiseaseRecord,
@@ -11,11 +13,18 @@ import {
 const router = express.Router();
 
 router.get("/infectious-record", getAllInfectiousDiseaseRecords); // lấy toàn bộ danh sách mắc bệnh truyền nhiễm
+router.get(
+  "/:student_id/infectious-record",
+  getInfectiousDiseaseRecordsOfStudent
+);
+
 router.get("/chronic-record", getAllChronicDiseaseRecords); // lấy toàn bộ danh sách mắc bệnh mãn tính
+router.get("/:student_id/chronic-record", getChronicDiseaseRecordsOfStudent);
 
 router.post("/student/:student_id/disease-record", createDiseaseRecord); // tạo mới bệnh cho cả truyền nhiễm và mãn tính
 router.patch("/student/:student_id/disease-record", updateDiseaseRecord); // update disease record
-router.get("/student/:student_id/disease-record", getDiseaseOfStudent); // list toàn bộ bệnh của một học sinh
+router.get("/student/:student_id/disease-record", getDiseaseRecordsOfStudent); // list toàn bộ bệnh của một học sinh
+router.get("/:student_id/disease-record", getDiseaseRecordsOfStudent); // parent list toàn bộ bệnh của một học sinh
 router.get("/disease-record", getAllDiseaseRecords); // lấy toàn bộ danh sách bệnh của tất cả học sinh
 
 export default router;
