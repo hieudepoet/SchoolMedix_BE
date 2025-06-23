4ae04f50bae015de7b51c712337fe4f10cbbbc8e
DuNguyenTruongThuanSE150361 <thuandntse150361@fpt.edu.vn>
2025-06-23 20:16:12 +0700

    Update Special Exam

--- src/controllers/specialistexam.controller.js
+++ src/controllers/specialistexam.controller.js
@@ -15,11 +15,48 @@ export async function getALLSpeciaListExamRecord(req, res) {
                 .json({ error: true, message: "Không tìm thấy Specialist Exam" });
         }
 
+        return res.status(200).json({
+            error: false,
+            data: rs,
+        });
+
+    } catch (err) {
+        console.error("❌ Error creating Campaign ", err);
         return res
-            .status(200)
-            .json({ error: false, message: "Get Specialist Exam thành công" });
+            .status(500)
+            .json({ error: true, message: "Lỗi khi lấy SpecialistExam." });
+    }
+
+}
 
 
+export async function createSpecialistExam(req, res) {
+    const { name, description } = req.body;
+
+    try {
+
+        if (!name || !description) {
+            return res
+                .status(400)
+                .json({ error: true, message: "Không Nhận được name or description" });
+        }
+
+        const rs = await query(`INSERT INTO SpecialistExamList 
+            (name,description) 
+            VALUES ($1,$2)`
+            , [name, description]);
+
+        if (rs.rowCount === 0) {
+            return res
+                .status(400)
+                .json({ error: true, message: "Tạo mới Specialist Exam không thành công" });
+        }
+
+        return res.status(200).json({
+            error: false,
+            message: "Tạo mới Specialis Exam thành công!",
+
+        });
 
 
 
@@ -30,4 +67,97 @@ export async function getALLSpeciaListExamRecord(req, res) {
             .json({ error: true, message: "Lỗi khi lấy SpecialistExam." });
     }
 
+
+
+}
+
+
+export async function updateSpecialExam(req, res) {
+
+    const { id } = req.params;
+    const { name, description } = req.body;
+
+    try {
+
+        if (!id || !name || !description) {
+            return res
+                .status(400)
+                .json({ error: true, message: "Không Nhận được ID or name or description" });
+        }
+
+        const check = await query(`SELECT * FROM specialistexamlist
+             WHERE id = $1`
+            , [id]);
+
+        if (check.rowCount === 0) {
+            return res
+                .status(400)
+                .json({ error: true, message: "Không Nhận tồn tại Special Exam" });
+        }
+
+        const rs = await query(`UPDATE specialistexamlist 
+            SET name = $1,description = $2 
+            WHERE id = $3`
+            , [name, description, id]);
+
+        if (rs.rowCount===0){
+            return res
+                .status(400)
+                .json({ error: true, message: "Update không thành công" });
+        }
+
+        return res.status(200).json({
+            error: false,
+            message: "Update thành công !",
+
+        });
+
+
+
+
+    } catch (err) {
+        console.error("❌ Error creating Campaign ", err);
+        return res
+            .status(500)
+            .json({ error: true, message: "Lỗi khi lấy SpecialistExam." });
+    }
+}
+
+
+export async function getSpecialExam(req,res) {
+   const { id } = req.params;
+    try{
+         if (!id ) {
+            return res
+                .status(400)
+                .json({ error: true, message: "Không Nhận được ID " });
+        }
+
+        const check = await query(`SELECT * FROM specialistexamlist
+             WHERE id = $1`
+            , [id]);
+        const rs = check.rows;
+
+        if (check.rowCount === 0) {
+            return res
+                .status(400)
+                .json({ error: true, message: "Không Nhận tồn tại Special Exam" });
+        }
+
+        return res.status(200).json({
+            error: false,
+            data:rs
+           
+
+        });
+
+
+
+
+    }catch (err) {
+        console.error("❌ Error creating Campaign ", err);
+        return res
+            .status(500)
+            .json({ error: true, message: "Lỗi khi lấy SpecialistExam." });
+    }
 }
--- src/routes/specialistExam.routes.js
+++ src/routes/specialistExam.routes.js
@@ -1,20 +1,26 @@
 import express from "express"
-import{ getALLSpeciaListExamRecord } from "../controllers/specialistexam.controller.js";
+import{ createSpecialistExam, getALLSpeciaListExamRecord, getSpecialExam, updateSpecialExam } from "../controllers/specialistexam.controller.js";
 
 const router = express.Router();
 
 
 // lấy toàn bộ chuyên khoa
 
-router.get('/special-exam-list',getALLSpeciaListExamRecord);
+router.get('/special-exam',getALLSpeciaListExamRecord);
 
 // lấy info toàn bộ của một chuyên khoa
 
+router.get('/special-exam/:id',getSpecialExam);
+
 // thêm mới một chuyên khoa
+
+router.post('/special-exam',createSpecialistExam);
 
 // delete một chuyên khoa (set is_deleted = true)
 
 // update thông tin của một chuyên khoa 
+
+router.patch('/special-exam/:id',updateSpecialExam); // Truyền vào Spe_id
 
 
 
