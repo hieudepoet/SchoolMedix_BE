77a361ccbb954837d01cf1645426c3709e78f6d8
Mai Nguyen Duy Khanh <mndkhanh@gmail.com>
2025-06-27 17:11:06 +0700

    change sql to run (user gender to isMale bool), add template for excel module

---
+++ src/services/excel/template-config.js
@@ -0,0 +1,15 @@
+export const USER_IMPORT_TEMPLATE = {
+      TOTAL_FIELD_NEEDED: 10,
+      COLUMN: [
+            "name",
+            "dob",
+            "address",
+            "class_id",
+            "mom_id",
+            "dad_id",
+            "email",
+            "year_of_enrollment",
+            "isMale",
+            "phone_number"
+      ]
+}
