64267e682b7617dabaa6690d1b94b0a3240344e9
Mai Nguyen Duy Khanh <mndkhanh@gmail.com>
2025-06-30 13:46:10 +0700

    users (70%) + class controller

---
+++ src/routes/class.routes.js
@@ -0,0 +1,14 @@
+import express from "express";
+import {
+      getInfoOfGrades,
+      getClassesInfoOfGradeByID,
+      getInfoOfClasses
+} from "../controllers/class.controller.js";
+
+const router = express.Router();
+
+router.get("/grade", getInfoOfGrades);
+router.get("/grade/:grade_id/class", getClassesInfoOfGradeByID);
+router.get("/class", getInfoOfClasses);
+
+export default router;
