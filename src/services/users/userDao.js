21698c8d16a32f2fcf4fcc90e1a4c6004bff524e
Viet Hieu Dinh <59277071+hieudepoet@users.noreply.github.com>
2025-07-07 09:47:31 +0700

    Update userDao.js

--- src/services/users/userDao.js
+++ src/services/users/userDao.js
@@ -1,5 +1,5 @@
 import { query } from "../../config/database.js";
-import { generateStudentCode, updateProfileFor } from "./userUtils.js"
+import { generateStudentCode, updateProfileFor } from "./userUtils.js";
 
 export async function insertAdmin(
   supabase_uid = null,
@@ -11,22 +11,25 @@ export async function insertAdmin(
   phone_number = null,
   profile_img_url = process.env.DEFAULT_AVATAR_URL
 ) {
-  const result = await query(`
+  const result = await query(
+    `
     INSERT INTO Admin (
       supabase_uid, email, name, dob, isMale, address,
       phone_number, profile_img_url
     ) VALUES ($1,$2,$3,$4,$5,$6,$7,$8)
     RETURNING *
-  `, [
-    supabase_uid,
-    email,
-    name,
-    dob,
-    isMale,
-    address,
-    phone_number,
-    profile_img_url
-  ]);
+  `,
+    [
+      supabase_uid,
+      email,
+      name,
+      dob,
+      isMale,
+      address,
+      phone_number,
+      profile_img_url,
+    ]
+  );
   return result.rows[0];
 }
 
@@ -40,22 +43,25 @@ export async function insertNurse(
   phone_number = null,
   profile_img_url = process.env.DEFAULT_AVATAR_URL
 ) {
-  const result = await query(`
+  const result = await query(
+    `
     INSERT INTO Nurse (
       supabase_uid, email, name, dob, isMale, address,
       phone_number, profile_img_url
     ) VALUES ($1,$2,$3,$4,$5,$6,$7,$8)
     RETURNING *
-  `, [
-    supabase_uid,
-    email,
-    name,
-    dob,
-    isMale,
-    address,
-    phone_number,
-    profile_img_url
-  ]);
+  `,
+    [
+      supabase_uid,
+      email,
+      name,
+      dob,
+      isMale,
+      address,
+      phone_number,
+      profile_img_url,
+    ]
+  );
   return result.rows[0];
 }
 
@@ -69,22 +75,25 @@ export async function insertParent(
   phone_number = null,
   profile_img_url = process.env.DEFAULT_AVATAR_URL
 ) {
-  const result = await query(`
+  const result = await query(
+    `
     INSERT INTO Parent (
       supabase_uid, email, name, dob, isMale, address,
       phone_number, profile_img_url
     ) VALUES ($1,$2,$3,$4,$5,$6,$7,$8)
     RETURNING *
-  `, [
-    supabase_uid,
-    email,
-    name,
-    dob,
-    isMale,
-    address,
-    phone_number,
-    profile_img_url
-  ]);
+  `,
+    [
+      supabase_uid,
+      email,
+      name,
+      dob,
+      isMale,
+      address,
+      phone_number,
+      profile_img_url,
+    ]
+  );
   return result.rows[0];
 }
 
@@ -104,7 +113,8 @@ export async function insertStudent(
 ) {
   const student_id = await generateStudentCode(year_of_enrollment);
 
-  const result = await query(`
+  const result = await query(
+    `
     INSERT INTO Student (
       id, supabase_uid, email, name, dob, isMale, address,
       phone_number, profile_img_url, year_of_enrollment, class_id, mom_id, dad_id
@@ -113,26 +123,46 @@ export async function insertStudent(
       $1, $2, $3, $4, $5, $6, $7,
       $8, $9, $10, $11, $12, $13
     ) RETURNING *
-  `, [
-    student_id, supabase_uid, email, name, dob, isMale, address,
-    phone_number, profile_img_url, year_of_enrollment, class_id, mom_id, dad_id
-  ]);
+  `,
+    [
+      student_id,
+      supabase_uid,
+      email,
+      name,
+      dob,
+      isMale,
+      address,
+      phone_number,
+      profile_img_url,
+      year_of_enrollment,
+      class_id,
+      mom_id,
+      dad_id,
+    ]
+  );
 
   return result.rows[0];
 }
 
 export async function getProfileOfAdminByID(admin_Id) {
-  const result = await query('select * from admin where id = $1 and is_deleted = false', [admin_Id]);
+  const result = await query(
+    "select * from admin where id = $1 and is_deleted = false",
+    [admin_Id]
+  );
   return result.rows[0];
 }
 
 export async function getProfileOfNurseByID(nurse_id) {
-  const result = await query('SELECT * FROM nurse WHERE id = $1 and is_deleted = false', [nurse_id]);
+  const result = await query(
+    "SELECT * FROM nurse WHERE id = $1 and is_deleted = false",
+    [nurse_id]
+  );
   return result.rows[0];
 }
 
 export async function getProfileOfParentByID(parent_id) {
-  const result = await query(` 
+  const result = await query(
+    ` 
   SELECT 
     p.id,
     p.supabase_uid,
@@ -169,17 +199,19 @@ export async function getProfileOfParentByID(parent_id) {
     ) AS children
 
   FROM parent p
-  where p.is_deleted = false
   LEFT JOIN student s ON s.mom_id = p.id OR s.dad_id = p.id
   LEFT JOIN class c ON c.id = s.class_id
   WHERE p.id = $1 and p.is_deleted = false
   GROUP BY p.id
-`, [parent_id]);
+`,
+    [parent_id]
+  );
   return result.rows[0];
 }
 
 export async function getProfileOfStudentByID(student_id) {
-  const result = await query(`
+  const result = await query(
+    `
 SELECT 
   s.id,
   s.supabase_uid,
@@ -236,7 +268,9 @@ LEFT JOIN parent d ON d.id = s.dad_id
 JOIN class c ON c.id = s.class_id
 WHERE s.id = $1 AND s.is_deleted = false;
 
-`, [student_id]);
+`,
+    [student_id]
+  );
   return result.rows[0];
 }
 // Admin
@@ -376,14 +410,17 @@ WHERE s.supabase_uid = $1 AND s.is_deleted = false;
   return result.rows[0];
 }
 
-
 export async function getAllAdmins() {
-  const result = await query('SELECT * FROM admin where is_deleted = false ORDER BY id ');
+  const result = await query(
+    "SELECT * FROM admin where is_deleted = false ORDER BY id "
+  );
   return result.rows;
 }
 
 export async function getAllNurses() {
-  const result = await query('SELECT * FROM nurse where is_deleted = false ORDER BY id');
+  const result = await query(
+    "SELECT * FROM nurse where is_deleted = false ORDER BY id"
+  );
   return result.rows;
 }
 
@@ -436,7 +473,7 @@ FROM (
 
   `);
 
-  return result.rows.map(row => row.parent_profile);
+  return result.rows.map((row) => row.parent_profile);
 }
 
 export async function getAllStudents() {
@@ -500,11 +537,12 @@ ORDER BY s.id;
 
   `);
 
-  return result.rows.map(row => row.student_profile);
+  return result.rows.map((row) => row.student_profile);
 }
 
 export async function getAllStudentsByClassID(class_id) {
-  const result = await query(`
+  const result = await query(
+    `
     SELESELECT json_build_object(
   'id', s.id,
   'supabase_uid', s.supabase_uid,
@@ -562,13 +600,16 @@ LEFT JOIN class c ON c.id = s.class_id
 WHERE s.is_deleted = false AND c.id = $1
 ORDER BY s.id;
 
-  `, [class_id]);
+  `,
+    [class_id]
+  );
 
-  return result.rows.map(row => row.student_profile);
+  return result.rows.map((row) => row.student_profile);
 }
 
 export async function getAllStudentsByGradeID(grade_id) {
-  const result = await query(`
+  const result = await query(
+    `
     SELECT json_build_object(
   'id', s.id,
   'supabase_uid', s.supabase_uid,
@@ -626,9 +667,11 @@ LEFT JOIN class c ON c.id = s.class_id
 WHERE s.is_deleted = false AND c.grade_id = $1
 ORDER BY s.id;
 
-  `, [grade_id]);
+  `,
+    [grade_id]
+  );
 
-  return result.rows.map(row => row.student_profile);
+  return result.rows.map((row) => row.student_profile);
 }
 
 export async function linkParentsAndStudents(mom_id, dad_id, student_ids) {
@@ -636,14 +679,17 @@ export async function linkParentsAndStudents(mom_id, dad_id, student_ids) {
     throw new Error("Danh sách học sinh không hợp lệ.");
   }
 
-  const result = await query(`
+  const result = await query(
+    `
     UPDATE student
     SET
       mom_id = COALESCE($1, mom_id),
       dad_id = COALESCE($2, dad_id)
     WHERE id = ANY($3::text[])
     RETURNING id, mom_id, dad_id
-  `, [mom_id, dad_id, student_ids]);
+  `,
+    [mom_id, dad_id, student_ids]
+  );
 
   return result.rows;
 }
@@ -664,7 +710,7 @@ export async function removeDadByStudentId(student_id) {
   return result.rows;
 }
 
-//---------------------------------------------------- update flow: 
+//---------------------------------------------------- update flow:
 
 // admin is able to update all
 export async function editUserProfileByAdmin(id, role, updates) {
@@ -673,76 +719,88 @@ export async function editUserProfileByAdmin(id, role, updates) {
 
 // below is used for self info modification
 export async function updateAdminProfile(id, updates) {
-  const allowedFields = ['address', 'profile_img_url', 'phone_number'];
+  const allowedFields = ["address", "profile_img_url", "phone_number"];
   const filteredUpdates = Object.fromEntries(
     Object.entries(updates).filter(([key]) => allowedFields.includes(key))
   );
 
   if (Object.keys(filteredUpdates).length === 0) {
-    throw new Error("Chỉ được phép cập nhật trường địa chỉ, ảnh đại diện và số điện thoại.");
+    throw new Error(
+      "Chỉ được phép cập nhật trường địa chỉ, ảnh đại diện và số điện thoại."
+    );
   }
   return await updateProfileFor(id, "admin", updates);
 }
 
 export async function updateNurseProfile(id, updates) {
-  const allowedFields = ['address', 'profile_img_url', 'phone_number'];
+  const allowedFields = ["address", "profile_img_url", "phone_number"];
   const filteredUpdates = Object.fromEntries(
     Object.entries(updates).filter(([key]) => allowedFields.includes(key))
   );
 
   if (Object.keys(filteredUpdates).length === 0) {
-    throw new Error("Chỉ được phép cập nhật trường địa chỉ, ảnh đại diện và số điện thoại.");
+    throw new Error(
+      "Chỉ được phép cập nhật trường địa chỉ, ảnh đại diện và số điện thoại."
+    );
   }
   return await updateProfileFor(id, "nurse", updates);
 }
 
 // parent và student chỉ được update address, profile_img_url, phone_number
 export async function updateStudentProfile(id, updates) {
-  const allowedFields = ['address', 'profile_img_url', 'phone_number'];
+  const allowedFields = ["address", "profile_img_url", "phone_number"];
   const filteredUpdates = Object.fromEntries(
     Object.entries(updates).filter(([key]) => allowedFields.includes(key))
   );
 
   if (Object.keys(filteredUpdates).length === 0) {
-    throw new Error("Chỉ được phép cập nhật trường địa chỉ, ảnh đại diện và số điện thoại.");
+    throw new Error(
+      "Chỉ được phép cập nhật trường địa chỉ, ảnh đại diện và số điện thoại."
+    );
   }
 
   return await updateProfileFor(id, "student", filteredUpdates);
 }
 
 export async function updateParentProfile(id, updates) {
-  const allowedFields = ['address', 'profile_img_url', 'phone_number'];
+  const allowedFields = ["address", "profile_img_url", "phone_number"];
   const filteredUpdates = Object.fromEntries(
     Object.entries(updates).filter(([key]) => allowedFields.includes(key))
   );
 
   if (Object.keys(filteredUpdates).length === 0) {
-    throw new Error("Chỉ được phép cập nhật trường địa chỉ, ảnh đại diện và số điện thoại.");
+    throw new Error(
+      "Chỉ được phép cập nhật trường địa chỉ, ảnh đại diện và số điện thoại."
+    );
   }
 
   return await updateProfileFor(id, "parent", filteredUpdates);
 }
 
-
-
-//---------------------------------------------------- end update flow: 
+//---------------------------------------------------- end update flow:
 export async function confirmEmailFor(role, id) {
-  const result = await query(`
+  const result = await query(
+    `
     update ${role}
     set email_confirmed = true
     where id = $1
     returning *
-    `, [id]);
+    `,
+    [id]
+  );
   return result.rows[0];
 }
 
 export async function unconfirmEmailFor(role, id) {
-  const result = await query(`
+  const result = await query(
+    `
     update ${role}
     set email_confirmed = false
     where id = $1
     returning *
-    `, [id]);
+    `,
+    [id]
+  );
   return result.rows[0];
 }
 
@@ -798,7 +856,6 @@ export async function getProfileByID(role, id) {
       throw new Error("Role không hợp lệ");
   }
 }
-
 
 export async function deleteUserByID(role, id) {
   const sql = `update ${role} set is_deleted = true, supabase_uid = null, email = null WHERE id = $1;`;
