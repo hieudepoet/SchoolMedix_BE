80639aa1993b5a44e6023d6181c1cfaf271fd0f4
Mai Nguyen Duy Khanh <mndkhanh@gmail.com>
2025-07-11 16:05:13 +0700

    add email confirm or unconfirm count for student dasboard

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -14,6 +14,8 @@ export async function getSummary(req, res) {
       newCasesResult,
       processingDrugResult,
       pendingDrugResult,
+      emailConfirmedStudentCountResult,
+      emailUnconfirmedStudentCountResult,
     ] = await Promise.all([
       query("SELECT COUNT(*) AS total FROM student"),
       query(`
@@ -60,6 +62,16 @@ export async function getSummary(req, res) {
         FROM SendDrugRequest 
         WHERE status = 'PROCESSING'    
       `),
+      query(`
+        SELECT COUNT(*) AS total
+        FROM student
+        WHERE email_confirmed = true
+      `),
+      query(`
+        SELECT COUNT(*) AS total
+        FROM student
+        WHERE email_confirmed = false
+      `),
     ]);
 
     // Extract the total count from each query result
@@ -68,28 +80,27 @@ export async function getSummary(req, res) {
       healthyStudents: parseInt(healthyStudentsResult.rows[0]?.total || 0, 10),
       recentAccidents: parseInt(recentAccidentsResult.rows[0]?.total || 0, 10),
       monitoredCases: parseInt(monitoredCasesResult.rows[0]?.total || 0, 10),
-      previousAccidents: parseInt(
-        previousAccidentsResult.rows[0]?.total || 0,
-        10
-      ),
+      previousAccidents: parseInt(previousAccidentsResult.rows[0]?.total || 0, 10),
       newCases: parseInt(newCasesResult.rows[0]?.total || 0, 10),
       proccessingDrug: parseInt(processingDrugResult.rows[0]?.total, 10) || 0,
       pendingDrug: parseInt(pendingDrugResult.rows[0]?.total || 0, 10),
+      emailConfirmedStudents: parseInt(emailConfirmedStudentCountResult.rows[0]?.total || 0, 10),
+      emailUnconfirmedStudents: parseInt(emailUnconfirmedStudentCountResult.rows[0]?.total || 0, 10),
       percent:
         parseInt(totalStudentsResult.rows[0]?.total || 0, 10) > 0
           ? Number(
-              (
-                (parseInt(healthyStudentsResult.rows[0]?.total || 0, 10) /
-                  parseInt(totalStudentsResult.rows[0]?.total || 0, 10)) *
-                100
-              ).toFixed(2)
-            )
+            (
+              (parseInt(healthyStudentsResult.rows[0]?.total || 0, 10) /
+                parseInt(totalStudentsResult.rows[0]?.total || 0, 10)) *
+              100
+            ).toFixed(2)
+          )
           : 0,
     };
 
     return res.status(200).json({
       error: false,
-      message: "Fetching data succesfully",
+      message: "Fetching data successfully",
       data: data,
     });
   } catch (error) {
@@ -100,6 +111,7 @@ export async function getSummary(req, res) {
     });
   }
 }
+
 
 export async function getAccidentStats(req, res) {
   try {
@@ -234,7 +246,7 @@ export async function getDiseaseStats(req, res) {
   }
 }
 
-export async function getHealthStats(req, res) {}
+export async function getHealthStats(req, res) { }
 
 export async function getMedicalPlans(req, res) {
   try {
@@ -379,29 +391,29 @@ export async function getHealthStatsByGradeID(req, res) {
     // Định dạng dữ liệu trả về
     const data = result.rows[0]
       ? {
-          checkupName: checkupName || "N/A",
-          latestCheckupDate: latestCheckupDate || "N/A",
-          totalChecked: parseInt(result.rows[0].total_checked, 10) || 0,
-          totalStudents: parseInt(result.rows[0].total_students, 10) || 0,
-          maleCount: parseInt(result.rows[0].male_count, 10) || 0,
-          femaleCount: parseInt(result.rows[0].female_count, 10) || 0,
-          maleHeightAvg: result.rows[0].male_height_avg
-            ? parseFloat(result.rows[0].male_height_avg.toFixed(1))
-            : null,
-          maleWeightAvg: result.rows[0].male_weight_avg
-            ? parseFloat(result.rows[0].male_weight_avg.toFixed(1))
-            : null,
-          femaleHeightAvg: result.rows[0].female_height_avg
-            ? parseFloat(result.rows[0].female_height_avg.toFixed(1))
-            : null,
-          femaleWeightAvg: result.rows[0].female_weight_avg
-            ? parseFloat(result.rows[0].female_weight_avg.toFixed(1))
-            : null,
-          grades: gradesResult.rows.map((row) => ({
-            id: row.id,
-            name: row.name,
-          })),
-        }
+        checkupName: checkupName || "N/A",
+        latestCheckupDate: latestCheckupDate || "N/A",
+        totalChecked: parseInt(result.rows[0].total_checked, 10) || 0,
+        totalStudents: parseInt(result.rows[0].total_students, 10) || 0,
+        maleCount: parseInt(result.rows[0].male_count, 10) || 0,
+        femaleCount: parseInt(result.rows[0].female_count, 10) || 0,
+        maleHeightAvg: result.rows[0].male_height_avg
+          ? parseFloat(result.rows[0].male_height_avg.toFixed(1))
+          : null,
+        maleWeightAvg: result.rows[0].male_weight_avg
+          ? parseFloat(result.rows[0].male_weight_avg.toFixed(1))
+          : null,
+        femaleHeightAvg: result.rows[0].female_height_avg
+          ? parseFloat(result.rows[0].female_height_avg.toFixed(1))
+          : null,
+        femaleWeightAvg: result.rows[0].female_weight_avg
+          ? parseFloat(result.rows[0].female_weight_avg.toFixed(1))
+          : null,
+        grades: gradesResult.rows.map((row) => ({
+          id: row.id,
+          name: row.name,
+        })),
+      }
       : {};
 
     return res.status(200).json({
