aa2d35331ae5955e85d41afc70243172f6c92410
Mai Nguyen Duy Khanh <mndkhanh@gmail.com>
2025-07-16 03:14:29 +0700

    asdf

--- src/services/pdf/index.js
+++ src/services/pdf/index.js
@@ -9,24 +9,14 @@ export async function generatePDFBufferFromHealthRecord(record) {
 
     const html = generateHealthRecordsHtmlFromJson(record);
 
-    const browser = await puppeteer.launch({
-        headless: 'new',
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    });
-
-    const page = await browser.newPage();
-    await page.setContent(html, { waitUntil: 'networkidle0' });
-
-    const pdfBuffer = await page.pdf({ format: 'A4', printBackground: true });
-
-    await browser.close();
+    const pdfBuffer = await generatePDFBufferByHTMLText(html);
     return pdfBuffer;
 }
 
 export async function generatePDFBufferForFinalHealthReport(campaign_info, student_profile, general_health, specialist_exam_records) {
     const html = generateFinalHealthReportHTML(campaign_info, student_profile, general_health, specialist_exam_records);
     const pdfBuffer = await generatePDFBufferByHTMLText(html);
-    return Buffer.from(pdfBuffer);
+    return pdfBuffer;
 }
 
 export async function generatePDFBufferByHTMLText(html) {
