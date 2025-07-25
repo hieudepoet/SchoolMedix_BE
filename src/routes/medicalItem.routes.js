fb5c76b9207ee912c653be92ab0349fbb400f0a4
Mai Nguyen Duy Khanh <mndkhanh@gmail.com>
2025-07-25 23:33:06 +0700

    add controller for manage medical item

--- src/routes/medicalItem.routes.js
+++ src/routes/medicalItem.routes.js
@@ -1,27 +1,24 @@
 import express from "express";
-import multer from "multer";
 import {
-     createBlog,
-     deleteBlog,
-     getAllBlog,
-     getBlogById,
-     updateBlog, 
-     uploadImgSupabase,
-     getBlogType
-} from "../controllers/blog.controller.js";
-
-const upload = multer();
+     getMedicalItemById,
+     getAllMedicalSupplies,
+     getAllMedications,
+     updateMedicalItem,
+     createNewMedication,
+     createNewMedicalSupply,
+} from "../controllers/medicalItem.controller.js";
 
 const router = express.Router();
 
-router.post('/upload-image', upload.array("files"), uploadImgSupabase);
-router.post('/created-blog', createBlog);
-router.put('/update-blog/:id', updateBlog);
-router.patch('/delete-blog/:id', deleteBlog);
-router.get('/blog', getAllBlog);
-router.get('/blog/:id', getBlogById);
-router.get('/blog-type',getBlogType);
+// quan ly thuoc vat tu
+router.get('/medical-item/:id', getMedicalItemById);  // Get item by ID
+router.get('/medical-supply', getAllMedicalSupplies);       // Get all medical supplies
+router.get('/medication', getAllMedications);        // Get all medications
+router.patch('/medical-item/:id', updateMedicalItem);     // Update name, unit, description, exp_date
+router.post('/medication', createNewMedication);        // Create medication with quantity = 0
+router.post('/medical-supply', createNewMedicalSupply);   // Create supply with quantity = 0
+
+// quan ly ncc, tiem thuoc, vat tu...
 
 
 export default router;
-
