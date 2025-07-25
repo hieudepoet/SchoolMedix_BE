7d89ea4e98c835ce38ff3d3f5d6d0b5d597eac54
Viet Hieu Dinh <59277071+hieudepoet@users.noreply.github.com>
2025-07-26 00:58:32 +0700

    Update disease.routes.js

--- src/routes/disease.routes.js
+++ src/routes/disease.routes.js
@@ -6,14 +6,21 @@ import {
   updateDisease,
   deleteDisease,
   getVaccinesByDisease,
+  getSetOfDisease,
 } from "../controllers/disease.controller.js";
 
 const router = express.Router();
 
 router.post("/disease", createDisease);
+router.get("/vaccine-disease", getSetOfDisease);
 router.get("/diseases", getAllDiseases);
 router.put("/disease/:id", updateDisease);
 router.delete("/disease/:id", deleteDisease);
-router.get("/diseases/:id/vaccines", getVaccinesByDisease);
+router.get("/diseases/vaccines", (req, res) => {
+  const { diseaseId } = req.query;
+  console.log(diseaseId);
+  // 5. Gọi hàm xử lý chính
+  getVaccinesByDisease(req, res, diseaseId);
+});
 
 export default router;
